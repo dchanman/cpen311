ecb756ee9304660896345abf359a4038bde52045
Grafne23 <Graeme@dhcp-206-87-128-212.ubcsecure.wireless.ubc.ca>
2015-09-29 18:52:48 -0700

    slight changes

--- lab2/dist_files/task5/roulette.vhd
+++ lab2/dist_files/task5/roulette.vhd
@@ -35,5 +35,84 @@ END roulette;
 
 
 ARCHITECTURE structural OF roulette IS
+  
+  component spinwheel IS
+	PORT(
+		fast_clock : IN  STD_LOGIC;  -- This will be a 27 Mhz Clock  component spinwheel IS
+	PORT(
+		fast_clock : IN  STD_LOGIC;  -- This will be a 27 Mhz Clock
+		resetb : IN  STD_LOGIC;      -- asynchronous reset
+		spin_result  : OUT UNSIGNED(5 downto 0));  -- current value of the wheel
+  END component;
+
+component win IS
+	PORT(spin_result_latched : in unsigned(5 downto 0);  -- result of the spin (the winning number)
+             bet1_value : in unsigned(5 downto 0); -- value for bet 1
+             bet2_colour : in std_logic;  -- colour for bet 2
+             bet3_dozen : in unsigned(1 downto 0);  -- dozen for bet 3
+             bet1_wins : out std_logic;  -- whether bet 1 is a winner
+             bet2_wins : out std_logic;  -- whether bet 2 is a winner
+             bet3_wins : out std_logic); -- whether bet 3 is a winner
+END win component;
+		resetb : IN  STD_LOGIC;      -- asynchronous reset
+		spin_result  : OUT UNSIGNED(5 downto 0));  -- current value of the wheel
+  END component;
+
+component win IS
+	PORT(spin_result_latched : in unsigned(5 downto 0);  -- result of the spin (the winning number)
+             bet1_value : in unsigned(5 downto 0); -- value for bet 1
+             bet2_colour : in std_logic;  -- colour for bet 2
+             bet3_dozen : in unsigned(1 downto 0);  -- dozen for bet 3
+             bet1_wins : out std_logic;  -- whether bet 1 is a winner
+             bet2_wins : out std_logic;  -- whether bet 2 is a winner
+             bet3_wins : out std_logic); -- whether bet 3 is a winner
+END win component;
+  
+  signal spin_result : unsigned(5 downto 0);
+  signal slow_clock : std_logic := '0';
+ 	signal spin_result_latched : unsigned(5 downto 0);  -- result of the spin (the winning number)
+  signal bet1_value : unsigned(5 downto 0); -- value for bet 1
+  signal bet2_colour : std_logic;  -- colour for bet 2
+  signal bet3_dozen : unsigned(1 downto 0);  -- dozen for bet 3
+  signal bet1_wins : std_logic;  -- whether bet 1 is a winner
+  signal bet2_wins : std_logic;  -- whether bet 2 is a winner
+  signal bet3_wins :  std_logic; -- whether bet 3 is a winner
  --- Your code goes here
+ begin
+  spinner : spinwheel port map(fast_clock => CLOCK_27, resetb => key(1), spin_result => spin_result);
+   
+  process(slow_clock) begin
+     if(rising_edge(slow_clock)) then
+      spin_result_latched <= spin_result;
+    end if;
+  end process;
+  
+    process(slow_clock) begin
+     if(rising_edge(slow_clock)) then
+      bet1_value <= unsigned(SW(8 downto 3));
+    end if;
+  end process;
+  
+  process(slow_clock) begin
+     if(rising_edge(slow_clock)) then
+      bet2_colour <= SW(12);
+    end if;
+  end process;
+  
+  process(slow_clock) begin
+     if(rising_edge(slow_clock)) then
+      bet3_dozen <= unsigned(SW(17 downto 16));
+    end if;
+  end process;
+  
+  win_block : win port map (spin_result_latched => spin_result_latched,
+             bet1_value => bet1_value,
+             bet2_colour => bet2_colour,
+             bet3_dozen => bet3_dozen,
+             bet1_wins => bet1_wins,
+             bet2_wins => bet2_wins,
+             bet3_wins => bet3_wins);
+    
+    
+    
 END;
