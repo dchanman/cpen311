6d4cd5bdfd79688ce31d75b535de43a06786c9d9
Derek Chan <derekchan1994@gmail.com>
2015-10-12 15:56:21 -0700

    Added lab3_clear_screen to main lab3

--- lab3/lab3.vhd
+++ lab3/lab3.vhd
@@ -3,61 +3,85 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity lab3 is
-  port(CLOCK_50            : in  std_logic;
-       KEY                 : in  std_logic_vector(3 downto 0);
-       SW                  : in  std_logic_vector(17 downto 0);
-       VGA_R, VGA_G, VGA_B : out std_logic_vector(9 downto 0);  -- The outs go to VGA controller
-       VGA_HS              : out std_logic;
-       VGA_VS              : out std_logic;
-       VGA_BLANK           : out std_logic;
-       VGA_SYNC            : out std_logic;
-       VGA_CLK             : out std_logic)
+	port(CLOCK_50						: in	std_logic;
+			 KEY								 : in	std_logic_vector(3 downto 0);
+			 SW									: in	std_logic_vector(17 downto 0);
+			 LEDG : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);  -- ledg
+			 VGA_R, VGA_G, VGA_B : out std_logic_vector(9 downto 0);	-- The outs go to VGA controller
+			 VGA_HS							: out std_logic;
+			 VGA_VS							: out std_logic;
+			 VGA_BLANK					 : out std_logic;
+			 VGA_SYNC						: out std_logic;
+			 VGA_CLK						 : out std_logic);
 end lab3;
 
 architecture rtl of lab3 is
 
  --Component from the Verilog file: vga_adapter.v
 
-  component vga_adapter
-    generic(RESOLUTION : string);
-    port (resetn                                       : in  std_logic;
-          clock                                        : in  std_logic;
-          colour                                       : in  std_logic_vector(2 downto 0);
-          x                                            : in  std_logic_vector(7 downto 0);
-          y                                            : in  std_logic_vector(6 downto 0);
-          plot                                         : in  std_logic;
-          VGA_R, VGA_G, VGA_B                          : out std_logic_vector(9 downto 0);
-          VGA_HS, VGA_VS, VGA_BLANK, VGA_SYNC, VGA_CLK : out std_logic);
-  end component;
+	component vga_adapter
+		generic(RESOLUTION : string);
+		port (resetn																			 : in	std_logic;
+					clock																				: in	std_logic;
+					colour																			 : in	std_logic_vector(2 downto 0);
+					x																						: in	std_logic_vector(7 downto 0);
+					y																						: in	std_logic_vector(6 downto 0);
+					plot																				 : in	std_logic;
+					VGA_R, VGA_G, VGA_B													: out std_logic_vector(9 downto 0);
+					VGA_HS, VGA_VS, VGA_BLANK, VGA_SYNC, VGA_CLK : out std_logic);
+	end component;
+	
+	component lab3_clear_screen is
+	port(
+		CLOCK	: in	std_logic;
+		RESET : in	std_logic;
+		START : in	std_logic;
+		COLOUR : out std_logic_vector(2 downto 0);
+		X : out std_logic_vector(7 downto 0);
+		Y : out std_logic_vector(6 downto 0);
+		PLOT	: out std_logic;
+		DONE	: out std_logic);
+	end component;
 
-  signal x      : std_logic_vector(7 downto 0);
-  signal y      : std_logic_vector(6 downto 0);
-  signal colour : std_logic_vector(2 downto 0);
-  signal plot   : std_logic;
+	signal x			: std_logic_vector(7 downto 0);
+	signal y			: std_logic_vector(6 downto 0);
+	signal colour : std_logic_vector(2 downto 0);
+	signal plot	 : std_logic;
 
 begin
 
-  -- includes the vga adapter, which should be in your project 
+	-- includes the vga adapter, which should be in your project 
 
-  vga_u0 : vga_adapter
-    generic map(RESOLUTION => "160x120") 
-    port map(resetn    => KEY(3),
-             clock     => CLOCK_50,
-             colour    => colour,
-             x         => x,
-             y         => y,
-             plot      => plot,
-             VGA_R     => VGA_R,
-             VGA_G     => VGA_G,
-             VGA_B     => VGA_B,
-             VGA_HS    => VGA_HS,
-             VGA_VS    => VGA_VS,
-             VGA_BLANK => VGA_BLANK,
-             VGA_SYNC  => VGA_SYNC,
-             VGA_CLK   => VGA_CLK);
+	vga_u0 : vga_adapter
+		generic map(RESOLUTION => "160x120") 
+		port map(resetn		=> KEY(3),
+						 clock		 => CLOCK_50,
+						 colour		=> SW(17 downto 15),
+						 x				 => x,
+						 y				 => y,
+						 plot			=> plot,
+						 VGA_R		 => VGA_R,
+						 VGA_G		 => VGA_G,
+						 VGA_B		 => VGA_B,
+						 VGA_HS		=> VGA_HS,
+						 VGA_VS		=> VGA_VS,
+						 VGA_BLANK => VGA_BLANK,
+						 VGA_SYNC	=> VGA_SYNC,
+						 VGA_CLK	 => VGA_CLK);
+						 
+	lab3_clear_screen_u1 : lab3_clear_screen
+		port map(
+			CLOCK	=> CLOCK_50,
+			RESET => KEY(0),
+			START => KEY(1),
+			COLOUR => colour,
+			X => x,
+			Y => y,
+			PLOT => plot,
+			DONE => LEDG(2));
 
 
-  -- rest of your code goes here, as well as possibly additional files
+	-- rest of your code goes here, as well as possibly additional files
 
 
 end RTL;
