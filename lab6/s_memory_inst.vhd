bc92cf65084f564bb4beda4589618a6c1062f9fe
Grafne23 <Graeme@Graemes-MacBook-Pro.local>
2015-11-28 14:49:35 -0800

    done phase 2 b)

---
+++ lab6/s_memory_inst.vhd
@@ -0,0 +1,7 @@
+s_memory_inst : s_memory PORT MAP (
+		address	 => address_sig,
+		clock	 => clock_sig,
+		data	 => data_sig,
+		wren	 => wren_sig,
+		q	 => q_sig
+	);
--- lab6/task1/ksa.vhd
+++ lab6/task1/ksa.vhd
@@ -7,7 +7,7 @@ use ieee.numeric_std.all;
 entity ksa is
   port(CLOCK_50 : in  std_logic;  -- Clock pin
        KEY : in  std_logic_vector(3 downto 0);  -- push button switches
-       SW : in  std_logic_vector(15 downto 0);  -- slider switches
+       SW : in  std_logic_vector(17 downto 0);  -- slider switches
 		 LEDG : out std_logic_vector(7 downto 0);  -- green lights
 		 LEDR : out std_logic_vector(17 downto 0));  -- red lights
 end ksa;
@@ -34,7 +34,7 @@ architecture rtl of ksa is
 	-- state names here as you complete your design
 	
 	type state_type is (state_init, 
-                       state_fill,						
+                       state_fill,	read_Si, waitState1, compute_j, waitState2, write_Si, write_Sj,					
    	 					  state_done);
 								
     -- These are signals that are used to connect to the memory													 
@@ -42,10 +42,16 @@ architecture rtl of ksa is
 	 signal data : STD_LOGIC_VECTOR (7 DOWNTO 0);
 	 signal wren : STD_LOGIC;
 	 signal q : STD_LOGIC_VECTOR (7 DOWNTO 0);	
+	 signal clk : std_logic;
+	 signal reset : std_logic;
+	 signal secret_key : std_logic_vector(23 downto 0);
 
 	 begin
+	   	clk <= CLOCK_50;
+	   	--clk <= KEY(1);
+	   	reset <= not(KEY(0));
+	   	secret_key <= "000000" & SW(17 downto 0);
 	    -- Include the S memory structurally
-	
        u0: s_memory port map (
 	        address, clk, data, wren, q);
 			  
@@ -56,7 +62,95 @@ architecture rtl of ksa is
        -- You will be likely writing this is a state machine. Ensure
        -- that after the memory is filled, you enter a DONE state which
        -- does nothing but loop back to itself.  
-
+       process(clk, reset)
+         variable state : state_type := state_init;
+         variable i : natural := 0;
+         variable j : natural := 0;
+         variable Si : natural := 0;
+         variable Sj : natural := 0;
+         begin
+          if(reset = '1') then
+           --reset
+           i := 0;
+           LEDG(7) <= '0';
+           state := state_init;
+          elsif rising_edge(clk) then
+           case state is
+           when state_init =>
+             i := 0;
+             state := state_fill;
+             --do stuff
+           when state_fill =>
+             if(i > 255) then 
+              state := read_Si;
+              i := 0;
+              j := 0;
+              wren <= '0';
+            else
+              state := state_fill;
+              address <= std_logic_vector(to_unsigned(i, address'length));
+              data <= std_logic_vector(to_unsigned(i, address'length));
+              wren <= '1';
+              i := i + 1;
+            end if;
+             --more stuff
+           when read_Si =>
+             if(i > 255) then
+              state := state_done;
+              wren <= '0';
+            else
+             address <= std_logic_vector(to_unsigned(i, address'length));
+             wren <= '0';
+             state := waitState1;
+            end if;
+           when waitState1 =>
+             wren <= '0';
+             state := compute_j;
+             Si := to_integer(unsigned(q));
+           when compute_j =>
+             wren <= '0';
+             Si := to_integer(unsigned(q));
+             j := j + Si;
+             case (i mod 3) is
+              when 0 => j := j + to_integer(unsigned(secret_key(23 downto 16)));
+              when 1 => j := j + to_integer(unsigned(secret_key(15 downto 8)));
+              when 2 => j := j + to_integer(unsigned(secret_key(7 downto 0)));
+              when others => j := j;
+            end case;
+           --j := j + to_integer(((unsigned(secret_key))(i mod 3)));
+             j := j mod 256;
+             address <= std_logic_vector(to_unsigned(j, address'length));
+             state := waitState2;
+           when waitState2 =>
+             state := write_Si;
+              Sj := to_integer(unsigned(q));
+           when write_Si =>
+               wren <= '1';
+              Sj := to_integer(unsigned(q));
+              address <= std_logic_vector(to_unsigned(j, address'length));
+              data <= std_logic_vector(to_unsigned(Si, address'length));
+              state := write_Sj;
+            when write_Sj =>
+              wren <= '1';
+              address <= std_logic_vector(to_unsigned(i, address'length));
+              data <= std_logic_vector(to_unsigned(Sj, address'length));
+              state := read_Si;
+              i := i + 1;
+           when state_done =>
+             state := state_done;
+             wren <= '0';
+             LEDG(7) <= '1';
+             --over
+           when others =>
+             --nothing
+           end case;
+          end if;
+          
+          LEDR(7 downto 0) <= std_logic_vector(to_unsigned(i, 8));
+          LEDR(17 downto 10) <= std_logic_vector(to_unsigned(j, 8));
+         end process;
+         
+  
 
 end RTL;
 
